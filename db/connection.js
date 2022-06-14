9c435eb8d19395d2019e0f810b9ad000b030a338
Qu33nArlene <ahaskins93@yahoo.com>
2022-06-14 17:16:44 -0400

    added votes table

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,12 @@
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-// Connect to database
 const db = mysql.createConnection({
-    host: 'localhost',
-    // Your MySQL username,
-    user: 'root',
-    // Your MySQL password 
-    password: 'Wh!sp3rR0ll!',
-    database: 'election'
-  });
+  host: 'localhost',
+  // Your MySQL username,
+  user: 'root',
+  // Your MySQL password
+  password: 'Wh!sp3rR0ll!',
+  database: 'election'
+});
 
-  module.exports = db;
-  
+module.exports = db;
